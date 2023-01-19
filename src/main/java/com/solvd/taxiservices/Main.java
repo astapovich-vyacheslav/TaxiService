a21b1a500326caaadde5e91f1b0fe6ed33a036de
Vyacheslav <astapovich_work@gmail.com>
2023-01-19 16:58:16 +0300

    updated hierarchy, added daos

---
+++ src/main/java/com/solvd/taxiservices/Main.java
@@ -0,0 +1,9 @@
+package com.solvd.taxiservices;
+
+import com.solvd.taxiservices.utils.JDBC.jdbc;
+
+public class Main {
+    public static void main(String[] args) {
+        jdbc.connect();
+    }
+}
